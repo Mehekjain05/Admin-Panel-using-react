d059c4d1e03316a941ddc666fc35cb08dd4282d3
mehekjain28@gmail.com <mehekjain28@gmail.com>
2022-08-10 19:45:08 +0530

    deploy my react project to github pages

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+ 
+function App(){
+ 
+        return (
+            <div>
+                <Navbar/>
+                <div class="container-fluid" id="main">
+                 <div class="row row-offcanvas row-offcanvas-left">
+                   <Sidebar/>
+                  <Dashboard/>
+                
+             </div>
+            </div>  
+        </div>  
+        );
+    }
+  
+export default App
